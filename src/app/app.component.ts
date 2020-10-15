29950108b5636459896a0464aecece7c79eb718d
unknown <adrianbanro@yahoo.fr>
2020-10-15 10:31:43 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { ProductService } from '././product.service';
+import { RatingComponent } from './rating.component';
+import { Rating22Component } from './rating22.component';
+import { UserFormComponent } from './user-form.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //templateUrl: './app.component.html',
+  template: `<h2>{{title}}</h2>
+  <img src="{{imageUrl}}" />
+  <button
+class="btn btn-primary"
+[class.disabled]="!isValid">Submit</button>
+
+<button (click)="onClickMe($event)">Submit</button>
+
+<user-form></user-form>
+
+
+<rating></rating>
+<rating22></rating22>
+
+  <products></products>` ,
+  styleUrls: ['./app.component.css'],
+  providers: [ProductService]
 })
 export class AppComponent {
-  title = 'Second';
+  title = 'Second997';
+  isValid = !false;
+  imageUrl = " https://www.w3schools.com/html/pic_mountain.jpg";
+
+  onClickMe($event){
+    console.log("Clicked77",$event)
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormcompComponent } from './formcomp/formcomp.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+
+import { RatingComponent } from './rating.component';
+import { Rating22Component } from './rating22.component';
+import { ProductComponent } from './product.component';
+import { UserFormComponent } from './user-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ProductsComponent, FormcompComponent, RatingComponent, Rating22Component, ProductComponent, UserFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
